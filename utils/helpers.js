d480ecf73683624d16e54cf0bfc8a15c538f3f43
WinnieCWu <winniechn7@gmail.com>
2022-04-05 19:54:42 -0700

    updated handlebars and homeroute

---
+++ utils/helpers.js
@@ -0,0 +1,25 @@
+//use methods built into Date object to avoid using unneeded dependencies
+module.exports = {
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
+  },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+    return word;
+  },
+  format_url: (url) => {
+    return (
+      url
+        //.replace returns the modified string
+        .replace("http://", "")
+        .replace("https://", "")
+        .replace("www.", "")
+        .split("/")[0]
+        .split("?")[0]
+    )
+  }
+}
